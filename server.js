c2efa1ba79374c6937e1d58b3b771cf7eb7673f1
Gilbert <gtrupere69@yahoo.com>
2018-01-03 10:21:37 +0800

    Code base for this project. Hoho...

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var http = require('http')
+var path = require('path')
+
+var app = express()
+var reload = require('reload');
+
+ var app = express()
+ app.get('/',function (req, res) {
+     res.sendFile(path.join(__dirname, '/index.html'))
+ })
+ app.set('port',process.env.PORT || 3000)
+ var server = http.createServer(app)
+ //reload lapha
+ reload(app);
+ server.listen(app.get('port'),function () {
+     console.log('Hi');
+     console.log('Web server listening on port ' + app.get('port'))
+ })
